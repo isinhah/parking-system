f95088f745ca8dd436d31dba43565cccca5b8696
Isabel Henrique <contato.isabelhenrique@gmail.com>
2025-02-07 18:33:18 -0300

    validação do dto de alteração de senha do usuário

--- src/main/java/com/spring/api/web/dto/UserPasswordDto.java
+++ src/main/java/com/spring/api/web/dto/UserPasswordDto.java
@@ -1,5 +1,6 @@
 package com.spring.api.web.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Builder
@@ -9,7 +10,10 @@ import lombok.*;
 @AllArgsConstructor
 public class UserPasswordDto {
 
+    @Size(min = 6, max = 6, message = "Password size must be exactly 6 characters")
     private String currentPassword;
+    @Size(min = 6, max = 6, message = "Password size must be exactly 6 characters")
     private String newPassword;
+    @Size(min = 6, max = 6, message = "Password size must be exactly 6 characters")
     private String confirmNewPassword;
 }
