37d32af015486d25e0f41aa5469e2f97c1b81ba5
Isabel Henrique <contato.isabelhenrique@gmail.com>
2025-02-07 19:05:03 -0300

    exceção tratada de validação de campos

--- src/main/java/com/spring/api/web/dto/UserCreateDto.java
+++ src/main/java/com/spring/api/web/dto/UserCreateDto.java
@@ -11,11 +11,11 @@ import lombok.Setter;
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class UserCreateDto {
 
-    @NotBlank
+    @NotBlank(message = "Username cannot be empty")
     @Email(message = "Email format is invalid", regexp = "^[a-z0-9.+-_]+@[a-z0-9.-]+\\.[a-z]{2,}$"
     )
     private String username;
-    @NotBlank
+    @NotBlank(message = "Password cannot be empty")
     @Size(min = 6, max = 6, message = "Password size must be exactly 6 characters")
     private String password;
     private String role;
--- src/main/java/com/spring/api/web/dto/UserPasswordDto.java
+++ src/main/java/com/spring/api/web/dto/UserPasswordDto.java
@@ -1,5 +1,6 @@
 package com.spring.api.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -10,10 +11,13 @@ import lombok.*;
 @AllArgsConstructor
 public class UserPasswordDto {
 
+    @NotBlank(message = "Current password cannot be empty")
     @Size(min = 6, max = 6, message = "Password size must be exactly 6 characters")
     private String currentPassword;
+    @NotBlank(message = "New password cannot be empty")
     @Size(min = 6, max = 6, message = "Password size must be exactly 6 characters")
     private String newPassword;
+    @NotBlank(message = "New password cannot be empty")
     @Size(min = 6, max = 6, message = "Password size must be exactly 6 characters")
     private String confirmNewPassword;
 }
