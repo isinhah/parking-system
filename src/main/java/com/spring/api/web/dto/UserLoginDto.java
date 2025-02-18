401c09ed91baf2b6cabf98564204f3cd26d3c60b
Isabel Henrique <contato.isabelhenrique@gmail.com>
2025-02-18 11:49:56 -0300

    controller de autenticação

---
+++ src/main/java/com/spring/api/web/dto/UserLoginDto.java
@@ -0,0 +1,24 @@
+package com.spring.api.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginDto {
+
+    @NotBlank(message = "Username cannot be empty")
+    @Email(message = "Email format is invalid", regexp = "^[a-z0-9.+-_]+@[a-z0-9.-]+\\.[a-z]{2,}$"
+    )
+    private String username;
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 6, max = 6, message = "Password size must be exactly 6 characters")
+    private String password;
+}
